18b52ea4ac7d28cc27b0e319662db092445d0265
Mohab <mohabahmedonem8@gmail.com>
2023-07-08 18:19:24 +0300

    Task2 finished

--- Task1/ARM7_LPC2129_Keil_RVDS/bin/portasm.d
+++
@@ -1,2 +0,0 @@
-.\bin\portasm.o: ..\..\..\..\free RTOS\FreeRTOS-Kernel-10.3.0\FreeRTOS\Source\portable\RVDS\ARM7_LPC21xx\portASM.s
-.\bin\portasm.o: ..\..\..\..\free RTOS\FreeRTOS-Kernel-10.3.0\FreeRTOS\Source\portable\RVDS\ARM7_LPC21xx\portmacro.inc
--- Task1/ARM7_LPC2129_Keil_RVDS/bin/serialisr.d
+++
@@ -1,2 +0,0 @@
-.\bin\serialisr.o: serial\serialISR.s
-.\bin\serialisr.o: ..\..\..\..\free RTOS\FreeRTOS-Kernel-10.3.0\FreeRTOS\Source\portable\RVDS\ARM7_LPC21xx\portmacro.inc
